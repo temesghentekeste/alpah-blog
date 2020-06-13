a00b0db1f70f53a5d06931ccf20c7aea8f661597
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-06-13 17:10:45 +0300

    Refactored ArticleController Created before_action to set_articles in edit, update, show and destroy actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
 
     def index
         @articles = Article.all
@@ -20,15 +21,12 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
 
         if @article.update(article_params)
             flash[:notice] = "Article was successfully updated"
@@ -39,13 +37,16 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = "Article was successfully deleted"
         redirect_to articles_path
     end
 
     private
+
+        def set_article
+            @article = Article.find(params[:id])
+        end
         def article_params
             params.require(:article).permit(:title, :description)
         end
