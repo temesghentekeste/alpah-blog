6aafcfc426b59de3c24e3b63493ab4b1cdc4b6e6
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-06-27 10:45:03 +0300

    Added article and category association, many to many

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_210146) do
+ActiveRecord::Schema.define(version: 2020_06_27_070648) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
