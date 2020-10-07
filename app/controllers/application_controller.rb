a94e6e3d11a9530dd7efa0f99c9f07f0bc1aefbf
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-07 23:13:51 +0300

    Update home page and add links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
 
+  before_action :require_user
   helper_method :current_user, :logged_in?
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -11,9 +13,9 @@ class ApplicationController < ActionController::Base
 
   def require_user
     if !logged_in?
-      flash[:danger] = "You must be logged in to perform this action"
+      flash[:notice] = "You must be logged in to perform this action"
       redirect_to root_path
     end
-
   end
+
 end
